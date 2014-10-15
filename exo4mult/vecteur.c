85345a724b35a0e2365e40eecb212684b7464a70
gauchya <antho2gauchy@gmail.com>
2014-10-15 22:59:14 +0200

    Exo4 sequentiel ok, creation dossier pour multi thread

--- exo4mult/vecteur.c
+++ exo4mult/vecteur.c
@@ -7,16 +7,18 @@
 
 struct vect_search {
 	int * tab;
+	int taille;
 	int element;
 };
 
-void *search (void *arg){
+void* search (void *arg){
 	struct vect_search * vecteur = (struct vect_search*)arg;
 	int i;
-	int trouve = 0;
-	for(i = 0 ; i < vecteur.length && !trouve; i++) {
+	int * trouve = malloc(sizeof(int));
+	*trouve = 0;
+	for(i = 0 ; i < vecteur->taille && !*trouve; i++) {
 		if(vecteur->tab[i] == vecteur->element) {
-			trouve = 1;
+			*trouve = 1;
 		}
 	}
 	
@@ -24,24 +26,22 @@ void *search (void *arg){
 }
 
 int main (int argc, char **argv){
-	int i;
-  FILE* fichier = NULL;
-	fichier = fopen(argv[1], "r+");
+  FILE* fichier = malloc(sizeof(FILE));
+	fichier = fopen (argv[1], "r+");
 	if (fichier != NULL) {
+		int i;
 		int taille_vecteur;
-		struct vect_search* vecteur;
+		struct vect_search* vecteur = malloc(sizeof(struct vect_search));
 		
 		fscanf(fichier, "%d\n", &taille_vecteur);
 		vecteur->tab = malloc(sizeof(int)*taille_vecteur);
+    vecteur->taille = taille_vecteur;
     
     for(i = 0 ; i < taille_vecteur ; i++) {
     	fscanf(fichier, "%d\n", &vecteur->tab[i]);
     }
-    
     vecteur->element = atoi(argv[2]);
-    
-    printf("%d",search(vecteur));
-    
+    printf("%d",*((int *)search(vecteur)));
     fclose(fichier);
   } else {
       printf("Erreur lors de l'ouverture du fichier %s", argv[1]);
