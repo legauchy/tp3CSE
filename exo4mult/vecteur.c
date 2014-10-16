7788095fa52710a50c55dd629e47bb43715bc935
gauchya <antho2gauchy@gmail.com>
2014-10-16 11:07:04 +0200

    Exo4mult terminé (SLPC)

--- exo4mult/vecteur.c
+++ exo4mult/vecteur.c
@@ -4,44 +4,78 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+#define NB_THREAD 8
 
-struct vect_search {
+
+struct vecteur {
 	int * tab;
 	int taille;
 	int element;
+	int * trouve;
+};
+
+struct vect_search {
+	struct vecteur * vect;
+	int debut_rech;
+	int fin_rech;
 };
 
 void* search (void *arg){
-	struct vect_search * vecteur = (struct vect_search*)arg;
-	int i;
-	int * trouve = malloc(sizeof(int));
-	*trouve = 0;
-	for(i = 0 ; i < vecteur->taille && !*trouve; i++) {
-		if(vecteur->tab[i] == vecteur->element) {
-			*trouve = 1;
+	struct vect_search * vect_search = (struct vect_search*) arg;
+	int i = 0;
+	for(i = vect_search->debut_rech ; i < vect_search->fin_rech && !*vect_search->vect->trouve; i++) {
+		if(vect_search->vect->tab[i] == vect_search->vect->element) {
+			*vect_search->vect->trouve = 1;
 		}
 	}
-	
-	return (void*)trouve;
+	free(vect_search);
+	return NULL;
+	//return (void*)(vect_search->vect->trouve);
 }
 
-int main (int argc, char **argv){
+int main (int argc, char **argv) {
   FILE* fichier = malloc(sizeof(FILE));
 	fichier = fopen (argv[1], "r+");
 	if (fichier != NULL) {
 		int i;
 		int taille_vecteur;
-		struct vect_search* vecteur = malloc(sizeof(struct vect_search));
+		struct vecteur * vecteur = malloc( sizeof(struct vecteur) );
 		
 		fscanf(fichier, "%d\n", &taille_vecteur);
-		vecteur->tab = malloc(sizeof(int)*taille_vecteur);
+		vecteur->tab = malloc( sizeof(int) * taille_vecteur );
     vecteur->taille = taille_vecteur;
     
     for(i = 0 ; i < taille_vecteur ; i++) {
     	fscanf(fichier, "%d\n", &vecteur->tab[i]);
     }
     vecteur->element = atoi(argv[2]);
-    printf("%d",*((int *)search(vecteur)));
+    vecteur->trouve = malloc(sizeof(int));
+    *vecteur->trouve = 0;
+    
+    pthread_t thread_tab[NB_THREAD];
+    int taille_secteur = vecteur->taille / NB_THREAD;
+    for (i = 0; i < NB_THREAD; i ++) {
+    	struct vect_search * vect_search = malloc(sizeof(struct vect_search));
+    	vect_search->vect = vecteur;
+    	vect_search->debut_rech = i * taille_secteur;
+    	vect_search->fin_rech = (i+1) * taille_secteur;
+    	if (vecteur->taille - vect_search->fin_rech < taille_secteur) {
+    		vect_search->fin_rech = vecteur->taille;
+    	}
+    	//printf("Debut %d, Fin %d\n", vect_search->debut_rech, vect_search->fin_rech);
+    	pthread_create (&thread_tab[i], NULL, search, (void *)vect_search);
+    }
+    
+    for (i = 0; i < NB_THREAD; i ++) {
+    	pthread_join (thread_tab[i], NULL);
+    }
+    
+    
+    printf("%d\n", *vecteur->trouve);
+    free(vecteur->tab);
+    free(vecteur->trouve);
+    free(vecteur);
+    
     fclose(fichier);
   } else {
       printf("Erreur lors de l'ouverture du fichier %s", argv[1]);
